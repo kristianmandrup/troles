2ce3dacba7f0b233fc57c1f66a100ff624d08908
Kristian Mandrup <kmandrup@gmail.com>
2011-06-07 00:08:42 +0200

    most of API specs are turned on and seem to work as they should :)

--- spec/troles/common/api/operations_api_spec.rb
+++ spec/troles/common/api/operations_api_spec.rb
@@ -1,6 +1,7 @@
 shared_examples_for "Common Operations API" do    
   describe '#list' do    
     it 'should clear the roles on clear!' do
+      user.clear_roles!      
       user.add_roles(:admin, :blogger)
       user.role_list.should include(:admin, :blogger)      
     end
@@ -8,15 +9,18 @@ shared_examples_for "Common Operations API" do
 
   describe '#clear!' do    
     it 'should clear the roles on clear!' do
-      expect { user.roles.clear! }.to change{ user.role_value }
+      user.roles.clear!
+      expect { user.role_list }.to change{user.role_list_value }      
       user.role_list.should be_empty      
     end
   end
 
   describe '#- operator' do
     it 'should remove the :admin role' do
+      user.clear_roles!      
       user.add_roles(:admin, :blogger)
-      expect { user.roles - :admin }.to change{ user.role_value }
+      user.roles - :admin
+      expect { user.role_list }.to change{user.role_list_value }
   
       user.has_role?(:blogger).should be_true      
       user.has_role?(:admin).should be_false
@@ -25,7 +29,9 @@ shared_examples_for "Common Operations API" do
   
   describe '#+ operator' do      
     it 'should add the :admin role' do
-      expect { user.roles + :admin }.to change{ user.role_value }
+      user.clear_roles!      
+      user.roles + :admin
+      expect { user.role_list }.to change{user.role_list_value }
       user.has_role?(:admin).should be_true      
     end
   end
@@ -33,7 +39,8 @@ shared_examples_for "Common Operations API" do
   describe '#<< operator' do
     it 'should add the :admin role' do
       user.clear_roles!
-      expect { user.roles << :admin }.to change{ user.role_value }
+      user.roles << :admin
+      expect { user.role_list }.to change{user.role_list_value }
       user.has_role?(:admin).should be_true      
     end
   end
@@ -41,7 +48,7 @@ shared_examples_for "Common Operations API" do
   describe 'acts as Enumerable' do
     it 'should enable include?' do
       user.clear_roles!
-      expect { user.roles << :admin }.to change{ user.role_value }
+      user.roles << :admin
       user.roles.include?(:admin).should be_true      
     end
   end

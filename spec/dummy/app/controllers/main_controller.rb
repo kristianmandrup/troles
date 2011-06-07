e690152b814513d665fd79b9b4a46958e8e683ff
Kristian Mandrup <kmandrup@gmail.com>
2011-06-07 10:34:24 +0200

    full dummy app integration test for AR works :)

--- spec/dummy/app/controllers/main_controller.rb
+++ spec/dummy/app/controllers/main_controller.rb
@@ -1,8 +1,16 @@
 class MainController < ApplicationController
 
   def index
-    @refmany_user = RefManyUser.first
-    @refmany_user.roles << :admin
-    puts @refmany_user.inspect    
+    @users = RefManyUser.all
+    
+    first_user = @users.first
+
+    # puts "Clearing roles of first user"
+    first_user.clear_roles!
+
+    # puts "Adding role admin to first user"
+    first_user.roles << :admin    
+        
+    # puts "#{first_user.name} has role :admin? #{first_user.has_role? :admin}"
   end
 end
--- spec/integration/troles/navigation_spec.rb
+++ spec/integration/troles/navigation_spec.rb
@@ -4,12 +4,14 @@ describe "Troles strategies" do
   include Capybara
   
   def user_setup
-    @user1 = RefmanyUser.create(:name => "Kris")
-    @user2 = RefmanyUser.create(:name => "Stanislaw")
-    @user3 = RefmanyUser.create(:name => "Miloviza")
+    RefManyUser.destroy_all
+    @user1 = RefManyUser.create(:name => "Kris")
+    @user2 = RefManyUser.create(:name => "Stanislaw")
+    @user3 = RefManyUser.create(:name => "Miloviza")
   end
   
   def role_setup
+    Role.destroy_all
     Role.create(:name => "user")
     Role.create(:name => "admin")
     Role.create(:name => "blogger")
@@ -26,7 +28,7 @@ describe "Troles strategies" do
   end
 
   it "setup should be right" do
-    RefmanyUser.all.size.should == 3
+    RefManyUser.all.size.should == 3
     Role.all.size.should == 4
   end
 
@@ -35,17 +37,12 @@ describe "Troles strategies" do
     response.status.should == 200
   end
 
-  describe "Refmany Strategy" do
-    describe "ActiveRecord" do
-      it "should read user records" do
-        @user1.roles.list.should be_empty
-      end
-    end
-
+  describe "RefMany Strategy" do
     it "should render user's name" do
-      get troles_refmany_path
+      get root_path
       #puts response.body
       response.body.should match(/Kris/)
+      response.body.should match(/Stanislaw/)      
     end
   end
 
